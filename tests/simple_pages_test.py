5961c60e9075fa8e9aab78dbb9e0e740ce767816
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2022-04-05 21:50:05 -0400

    Add files via upload

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,31 +4,24 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'href="/about"' in response.data
-    assert b'href="/welcome"' in response.data
-    assert b'href="/login"' in response.data
-    assert b'href="/register"' in response.data
-
-
-def test_request_index(client):
-    """This makes the index page"""
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Index" in response.data
+    assert b'<li><a href="/page/about">About</a></li>' in response.data
+    assert b'<li><a href="/page/welcome">Welcome</a></li>' in response.data
 
 def test_request_about(client):
     """This makes the index page"""
-    response = client.get("/about")
+    response = client.get("/page/about")
     assert response.status_code == 200
-    assert b"About" in response.data
+    assert b"About Page" in response.data
 
-def test_request_page1(client):
+def test_request_welcome(client):
     """This makes the index page"""
-    response = client.get("/welcome")
+    response = client.get("/page/welcome")
     assert response.status_code == 200
-    assert b"welcome" in response.data
+    assert b"Welcome Page" in response.data
+
 
 def test_request_page_not_found(client):
     """This makes the index page"""
-    response = client.get("/page5")
+    response = client.get("/page/page5")
     assert response.status_code == 404
+
